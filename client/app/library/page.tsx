793d7b75e7f5a3b5e1aeda8fffcee0c5c618f711
Shrestha Shashank <97455610+Shresth72@users.noreply.github.com>
2024-06-15 21:12:39 +0530

    shadcn fixed

--- client/app/library/page.tsx
+++ client/app/library/page.tsx
@@ -14,18 +14,18 @@ import PageTitle from "@/components/PageTitle";
 
 type Props = {};
 
-export default function UsersPage({}: Props) {
-    return (
-        <div>
-            <PageTitle title="Library" className="pb-10 text-5xl font-normal" />
-            <div className="flex flex-row gap-5  w-full">
-                <Button variant="outline" className="w-40 py-12 ">
-                    Upload Video
-                </Button>
-                <Button variant="outline" className="w-40 py-12">
-                    Create Folder
-                </Button>
-            </div>
-        </div>
-    );
+export default function UsersPage({ }: Props) {
+  return (
+    <div>
+      <PageTitle title="Library" className="pb-10 text-5xl font-normal" />
+      <div className="flex flex-row gap-5  w-full">
+        <Button variant="outline" className="w-40 py-12 ">
+          Upload Video
+        </Button>
+        <Button variant="outline" className="w-40 py-12">
+          Create Folder
+        </Button>
+      </div>
+    </div>
+  );
 }
