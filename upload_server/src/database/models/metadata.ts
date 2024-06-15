280b96c0b9866174cbd910ab1d752db52db47d2a
Md Kaif <mkaif2196@gmail.com>
2024-06-16 00:18:14 +0530

    metadata

--- upload_server/src/database/models/metadata.ts
+++ upload_server/src/database/models/metadata.ts
@@ -3,10 +3,31 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const MetadataSchema = new Schema({
-  title: String,
-  desc: String,
-  url: String,
-  // todo: Add more
+    title: {
+        type: String,
+        required: true,
+    },
+    desc: {
+        type: String,
+        required: true,
+    },
+    url: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: String,
+        required: false,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now,
+    },
+    // todo: Add more
 });
 
 export default mongoose.model("metadata", MetadataSchema);
